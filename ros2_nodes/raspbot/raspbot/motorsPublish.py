b08dadce36273e2142b52fef58772c18adcf7bf2
some45bucks <jhaightsnake@gmail.com>
2023-09-26 22:06:56 -0600

    test 11

--- ros2_nodes/raspbot/raspbot/motorsPublish.py
+++ ros2_nodes/raspbot/raspbot/motorsPublish.py
@@ -12,14 +12,19 @@ class MinimalPublisher(Node):
     self.publisher = self.create_publisher(Int32MultiArray, '/motor_control', 10)
     timer_period = 0.1 # seconds between scans
     self.timer = self.create_timer(timer_period, self.motor_callback)
+    self.stop = False
   
   def motor_callback(self):
+    if self.stop:
+      return
     msg = Int32MultiArray()
     msg.data = [100,100]
     self.publisher.publish(msg)
     sleep(2)
     msg.data = [0,0]
     self.publisher.publish(msg)
+    self.stop = True
+
 
 
 
@@ -29,6 +34,8 @@ def main(args=None):
   publisher = MinimalPublisher()
   try:
     rclpy.spin(publisher)
+  except KeyboardInterrupt as e:
+    print(e)
   except Exception as e:
     print(e)
   
--- ros2_nodes/raspbot/raspbot/motorsSubscribe.py
+++ ros2_nodes/raspbot/raspbot/motorsSubscribe.py
@@ -72,7 +72,6 @@ class MinimalSubscriber(Node):
     # self.servo2_angle = -1
   
   def motor_callback(self, msg):
-    print(msg)
     self.car.control_car(msg.data[0], msg.data[1])
   
   # def servo_callback(self, msg):
