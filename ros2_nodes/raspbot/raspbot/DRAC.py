45e9cf7c85f14cb898f6a9d77b092acf65af2f2f
Kevin Vulcano <kevin.vulcano@aggiemail.usu.edu>
2023-09-27 18:18:56 -0600

    drac fc

--- ros2_nodes/raspbot/raspbot/DRAC.py
+++ ros2_nodes/raspbot/raspbot/DRAC.py
@@ -12,22 +12,23 @@ class DRAC(Node):
     timer_period = 0.1
     self.timer = self.create_timer(timer_period, self.drac_callback)
     self.stop = False
-    self.turn_left = [75,-75]
+    self.turn_left = [-75,75]
     self.go_straight = [75,75]
 
     self.isTurning = False
     self.elapsed_time  = 0.0
     self.straight_duration  = 2.0
     self.turn_duration = 1.8
+    self.turn_count = 0
   
   def drac_callback(self):
-    if self.stop:
+    if self.stop or self.turn_count >= 3:
       return
     
     msg = Int32MultiArray()
     msg.data = self.motor_data()
     self.elapsed_time += 0.1
-    self.publisher.publish(msg)
+    self.publisher.publish(msg)    
     
   def motor_data(self):
     if self.isTurning:
@@ -45,6 +46,7 @@ class DRAC(Node):
   # straight -> turn or continue straight
   def go_straight_data(self):
     if self.elapsed_time >= self.straight_duration:
+      self.turn_count += 1
       self.isTurning = True
       self.elapsed_time = 0.0
     return self.go_straight
