4243475cc2ce1c9e35e4423c908985bc1429af42
Kyler Nelson <kyler.nelson@usu.edu>
2023-09-28 18:39:26 -0600

    Implement teleop client keyboard reading

--- teleop-controller-pc/teleop-client.py
+++ teleop-controller-pc/teleop-client.py
@@ -1,86 +1,94 @@
-from pynput.keyboard import Key, Listener as KeyboardListener
+from pynput.keyboard import Key, KeyCode, Listener as KeyboardListener
 
-key_down_set = set()
-moving_linearly = False
-turning = False
+last_key_pressed = None
+is_key_down = False
 
 def on_forward():
-    global moving_linearly
-    if moving_linearly:
-        return
-    moving_linearly = True
-    
     print("Moving forward...")
 
 def on_backward():
-    global moving_linearly
-    if moving_linearly:
-        return
-    moving_linearly = True
-    
     print("Moving backward...")
 
 def on_left():
-    global turning
-    if turning:
-        return
-    turning = True
-    
     print("Turning left...")
 
 def on_right():
-    global turning
-    if turning:
-        return
-    turning = True
-    
     print("Turning right...")
 
 def stop_robot():
+    print('Stop')
     pass
 
 def on_press(key):
-    if (key in key_down_set):
+    global last_key_pressed
+    global is_key_down
+
+    if (key == None or is_key_down):
         return
 
-    key_down_set.add(key)
-    if key == Key.w:
-        on_forward()
-    elif key == Key.s:
-        on_backward()
-    elif key == Key.a:
-        on_left()
-    elif key == Key.d:
-        on_right()
+    match key:
+        case KeyCode():
+            pressed_key = key.char
+        case _ as pressed_key:
+            pass
+
+    match pressed_key:
+        case 'w':
+            on_forward()
+        case 's':
+            on_backward()
+        case 'a':
+            on_left()
+        case 'd':
+            on_right()
+        case Key.esc:
+            return False
+        case _:
+            return
     
+    last_key_pressed = pressed_key
+    is_key_down = True
+
 
 def on_release(key):
-    global moving_linearly
-    global turning
-
-    key_down_set.remove(key)
+    global is_key_down
+    global last_key_pressed
+    
+    if key == None:
+        return
+    
     match key:
-        case Key.s, Key.w:
-            moving_linearly = False
-        case Key.a, Key.d:
-            turning = False
+        case KeyCode():
+            released_key = key.char
+        case _ as released_key:
+            pass
+        
+    if (released_key == last_key_pressed):
+        last_key_pressed = None
+        is_key_down = False
+        stop_robot()
+
 
 def print_controls():
     print("Teleop Client")
     print("Press 'w' to move forward")
-    print("Press 's' to move backward")
     print("Press 'a' to move left")
+    print("Press 's' to move backward")
     print("Press 'd' to move right\n")
-    print("To avoid cluttering up your terminal, you can take focus off your terminal.")
+    print("Commands will be sent to the robot as long as this process is running.")
 
 def main():
     print_controls()
     with KeyboardListener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
+    exit()
+
+def exit():
+    print("\nTerminating Teleop Client and Stopping Robot...")
+    stop_robot()
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print("\nTerminating Teleop Client and Stopping Robot...")
-        stop_robot()
+        exit()
